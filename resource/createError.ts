33f9d458fcbd75a56914e3fe287abc1bdece0bb2
kukuhpro <kukuhpro@gmail.com>
2021-01-24 23:45:51 +0700

    test: add jest typescript with automocking on interface handle

--- resource/createError.ts
+++ resource/createError.ts
@@ -1,5 +1,3 @@
-import { } from 'express'
-
 export interface rpcError extends Error {
     rpcError: boolean
     code: number
@@ -7,21 +5,13 @@ export interface rpcError extends Error {
     handled: boolean
 }
 
-interface rpcErrorConstructor {
-    new(message?: string): rpcError;
-    (message?: string): rpcError;
-    readonly prototype: rpcError;
-}
-
-let rpcErrorContstruct: rpcErrorConstructor;
-
 export default ({
     message,
     code = 1000,
     statusCode = 400,
     handled = true,
 }): rpcError => {
-    const error: rpcError = new rpcErrorContstruct(message)
+    const error: any = new Error(message)
     error.rpcError = true
     error.code = code
     error.statusCode = statusCode
---
+++ test/createError.test.ts
@@ -0,0 +1,70 @@
+import { createMock } from 'ts-auto-mock'
+import { On, method } from 'ts-auto-mock/extension'
+
+import faker from 'faker'
+
+
+import createError, { rpcError } from '../resource/createError'
+
+describe('createError', () => {
+    test('should create an error with default status code', () => {
+        const message: string = faker.random.words()
+        const error: rpcError = createError({ message })
+
+        expect(error.message).toBe(message)
+        expect(error.statusCode).toBe(400)
+        expect(error.handled).toBe(true)
+        expect(error.rpcError).toBe(true)
+    })
+
+    test('should create an error with a custom status code', () => {
+        const message: string = faker.random.words()
+        const statusCode: number = faker.random.number()
+        const error = createError({
+            message,
+            statusCode,
+        })
+        expect(error.message).toBe(message)
+        expect(error.statusCode).toBe(statusCode)
+        expect(error.handled).toBe(true)
+        expect(error.rpcError).toBe(true)
+    })
+
+    test('should create an error with default code', () => {
+        const message: string = faker.random.words()
+        const code: number = faker.random.number()
+        const error = createError({
+            message,
+        })
+        expect(error.message).toBe(message)
+        expect(error.code).toBe(code)
+        expect(error.handled).toBe(true)
+        expect(error.rpcError).toBe(true)
+    })
+
+    test('should create an error with custom code', () => {
+        const message: string = faker.random.words()
+        const code: number = faker.random.number()
+        const error = createError({
+            message,
+            code,
+        })
+        expect(error.message).toBe(message)
+        expect(error.code).toBe(code)
+        expect(error.handled).toBe(true)
+        expect(error.rpcError).toBe(true)
+    })
+
+    test('should create an error that is handled = false', () => {
+        const message: string = faker.random.words()
+        const handled: boolean = faker.random.number()
+        const error = createError({
+            message,
+            handled,
+        })
+        expect(error.message).toBe(message)
+        expect(error.statusCode).toBe(400)
+        expect(error.handled).toBe(handled)
+        expect(error.rpcError).toBe(true)
+    })
+})
